95ee83722c61bc3016d0447f987451637322c0fa
Lucas <lucasparteka@gmail.com>
2019-01-21 15:52:47 -0200

    modal para mostrar lista ordenada ja finalizado. Melhorias na função drag and drop dos atributos ordenaveis

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,12 +2,14 @@ class BookController {
 
     constructor() {
         this.booksOrder();
+        this.addBook();
+        this.removeModalLines();
     }
 
     booksOrder() {
         let valuesAvailables = [['title', '>'], ['title', '<'], ['author', '>'], ['author', '<'], ['year', '>'], ['year', '<']];
         let valuesSelected = [];
-
+        var arrayBooks = new Array;
         document.querySelector('#ordenar').addEventListener('click', () => {
             let selectedAttributes = document.querySelectorAll('.divsDrop .card .custom-control-input');
             selectedAttributes.forEach((bookAttribute) => {
@@ -16,7 +18,7 @@ class BookController {
                 }
             });
 
-            var arrayBooks = this.loadBooksArray();
+            arrayBooks = this.loadBooksArray();
             arrayBooks.sort(function (a, b) {
 
                 for (let i in valuesSelected) {
@@ -31,16 +33,43 @@ class BookController {
                 }
 
             });
-            console.log(arrayBooks);
+
+            for (let i in arrayBooks) {
+                let book = arrayBooks[i];
+                let tr = document.createElement('tr');
+                tr.innerHTML += `
+                    <tr>
+                        <td>${book.id}</td>
+                        <td>${book.title}</td>
+                        <td>${book.author}</td>
+                        <td>${book.year}</td>
+                    </tr>
+                `;
+                document.querySelector("#sorted-table").appendChild(tr);
+            }
         });
-        
+
+    }
+
+    removeModalLines() {
+
+        console.log('olá');
+
+        $(document).ready(function () {
+            $("#modal-center").on('hidden.bs.modal', function () {
+                document.querySelector('#sorted-table').innerHTML = "";
+                this.arrayBooks.splice(0, arrayBooks.length);
+                console.log(arrayBooks.length)
+                this.valuesSelected.splice(0, valuesSelected.length);
+                console.log(valuesSelected.length);
+            });
+        });
     }
 
     loadBooksArray() {
         let book;
         let books = [];
         let a = document.querySelectorAll('tbody tr');
-        //let td = tr.getElementByTagName('td');
         let b = [...a];
         for (let i in b) {
             let idBook = b[i].getElementsByTagName("td")[0].innerHTML;
@@ -53,31 +82,23 @@ class BookController {
         return books;
     }
 
-    /* bookArrayOrder(array, attribute, operator) {
- 
-        let arrayOrder = array.slice();
-        switch (operator) {
-            case '>':
-                arrayOrder.sort((a, b) => {
-                    if (a[attribute] < b[attribute]) return -1;
-                    if (a[attribute] > b[attribute]) return 1;
-                    return 0;
-                })
-                break;
-            case '<':
-                arrayOrder.sort((a, b) => {
-                    if (a[attribute] > b[attribute]) return -1;
-                    if (a[attribute] < b[attribute]) return 1;
-                    return 0;
-                })
-                break;
-            default:
-                console.log('nothing');
-        }
- 
-        //console.log(array)
-        return arrayOrder;
+    addBook() {
+        document.querySelector('#btn-add-book').addEventListener('click', () => {
+
+            let tr = document.createElement('tr');
+
+            tr.innerHTML += `
+        <tr>
+            <td>${document.querySelector("#field-id").value}</td>
+            <td>${document.querySelector("#field-title").value}</td>
+            <td>${document.querySelector("#field-author").value}</td>
+            <td>${document.querySelector("#field-year").value}</td>
+         </tr>
+        `;
+
+            document.querySelector("tbody").appendChild(tr);
+            document.querySelector("#form-add-book").reset();
+        })
     }
- */
 }
 
